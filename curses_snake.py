4f238ee78f04cf0803892e99b39d404594dc837b
txtyb <txtybbill@163.com>
2019-01-31 23:55:04 +0800

    Try to imitate and type a snake.

---
+++ curses_snake.py
@@ -0,0 +1,47 @@
+import curses
+from time import sleep
+
+# initialize the screen
+s = curses.initscr()
+curses.curs_set(0)
+curses.start_color()
+
+# initialize the window
+hei, wei = s.getmaxyx()
+w = curses.newwin(hei, wei, 0, 0)
+w.keypad(1)
+w.timeout(100)
+# initialize color_pairs
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
+curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_WHITE)
+
+# initialize the position of snake
+y = int(hei/2)
+x = int(wei/4)
+snake =[[y, x],[y, x-1],[y, x-2]]
+
+# initialize the position of food
+food_pos = [int(hei/2), int(wei/2)]
+w.addch(food_pos[0], food_pos[1], '✦',curses.color_pair(1))
+
+# start
+key = ord('d')
+next_key = w.getch()
+if next_key != -1:
+    if key == ord('a') and next_key != ord('d') or key == ord('d') and next_key != ord('a') or key == ord('w') and next_key != ord('s') or key == ord('s') and next_key != ord('w'):
+        key = next_key
+
+# death
+if snake[0][0] in [0, hei] or snake[0][1] in [0,wei] or snake[0] in snake[1:]:
+    w.clear()
+    for i in range(0, 2):
+        curses.flash()
+        sleep(0.08)
+    w.addstr(lnt(hei/2), int(wei/2), 'Game Over!', curses.color_pair(2))
+    sleep(0.5)
+    curses.endwin()
+    quit()
+
+# update the snake
+
