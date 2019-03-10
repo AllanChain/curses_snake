f8e1b3af5f03e9c8b600afcd4ac676a90c20ebcf
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2019-03-10 13:45:17 +0800

    add quikening and width auto calculation

--- curses_snake.py
+++ curses_snake.py
@@ -4,12 +4,14 @@ from random import randint
 
 
 class TimeBar:
-    def __init__(self, limit, MAX, x):
+    def __init__(self, limit, MAX, x, delay):
         self.LIMIT = 100
         self.unit = limit / MAX
         self.head =self.MAX = MAX
         self.x = x
         self.count = 0
+        self.delay = delay
+        w.timeout(delay)
         self.refill()
 
     def reduce(self):
@@ -25,6 +27,8 @@ class TimeBar:
         for y in range(self.MAX):
             w.addch(y, self.x, ' ', curses.color_pair(1))
         self.head = self.MAX
+        self.delay = int(self.delay * 0.98)
+        w.timeout(self.delay)
 
 
 def draw_block(pos, color=1):
@@ -45,9 +49,9 @@ def init():
     global hei, wei, w
     curses.resize_term(40, 81)
     hei, wei = s.getmaxyx()
+    wei = wei -1 if wei % 2 == 0 else wei
     w = curses.newwin(hei, wei, 0, 0)
     w.keypad(1)
-    w.timeout(100)
     wei = wei//2
     # initialize color_pairs
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
@@ -65,10 +69,6 @@ def init():
     food_pos = [int(hei/2), int(wei/2)]
     draw_block(food_pos)
     # the initial direction
-    global key
-    key = ord('d')
-    global keys
-    keys = [ord('w'), ord('a'), ord('s'), ord('d')]
 
 
 def death():
@@ -90,7 +90,7 @@ def main():
     global keys
     global food_pos
     global snake
-    timer = TimeBar(100, hei-2, 2*wei)
+    timer = TimeBar(100, hei-2, 2*wei, 150)
     while True:
         next_key = w.getch()
         timer.reduce()
@@ -146,6 +146,8 @@ def main():
         draw_block(snake[0], 3)
 
 
+key = ord('d')
+keys = [ord('w'), ord('a'), ord('s'), ord('d')]
 if __name__ == '__main__':
     try:
         init()
